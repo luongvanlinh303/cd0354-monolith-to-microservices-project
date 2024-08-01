c66c0305adb90f58032405c38b7dd49279236b6d
Luong Van Linh <luongvanlinh12@gmail.com>
2024-08-01 22:47:09 +0700

    fix docker file

--- set_env.sh
+++ set_env.sh
@@ -2,7 +2,7 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
 export POSTGRES_PASSWORD=123456789
-export POSTGRES_HOST=localhost
+export POSTGRES_HOST=database-1.cwwxxbpbkbva.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=udagram3
 export AWS_BUCKET=arn:aws:s3:::udagram3
 export AWS_REGION=us-east-1
--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -1,6 +1,6 @@
 import {Sequelize} from 'sequelize-typescript';
-import {config} from './config/config';
 
+import {config} from './config/config';
 
 export const sequelize = new Sequelize({
   'username': config.username,
@@ -10,4 +10,10 @@ export const sequelize = new Sequelize({
 
   'dialect': config.dialect,
   'storage': ':memory:',
+  dialectOptions: {
+    ssl: {
+      require: true, // This will help you. But you will see nwe error
+      rejectUnauthorized: false // This line will fix new error
+    }
+  },
 });
